2e47882e40ecbbe3609a4fa97a106f6465576675
Kharitonov Konstantin <karlyuhenson@gmail.com>
2020-05-19 14:37:08 +0300

    three commit

--- src/main/java/ReadTextsFile.java
+++ src/main/java/ReadTextsFile.java
@@ -8,10 +8,10 @@ public class ReadTextsFile {
     static void scannerFile () {
         System.out.println ( "Введите путь до файла" );
         scFile = new Scanner ( System.in );
-        File file = new File ( scFile.nextLine ( ));
+        File file = new File ( scFile.nextLine ( ) );
         try {
             scFile = new Scanner ( file );
-        } catch (FileNotFoundException ex) {
+        } catch ( FileNotFoundException exception ) {
             System.out.println ( "Системе не удается найти указанный путь" );
             scannerFile ( );
         }
@@ -19,43 +19,43 @@ public class ReadTextsFile {
 
     public static void main ( String[] args ) {
         scannerFile ( );
-        Map<String, Integer> map = new TreeMap <> ();
-        for ( Scanner scannerText = scFile; scannerText.hasNext ( ); ) {
+        Map < String, Integer > map = new TreeMap <> ( );
+        for ( Scanner scannerText = scFile ; scannerText.hasNext ( ) ; ) {
             String text = scFile.next ( ).replaceAll ( "\\," , "" ).
-                    replaceAll(" ", "").
-                    replaceAll("\\!", "").
-                    replaceAll( "\\?", "").
-                    replaceAll("\n", "").
-                    replaceAll("\\-", "").
-                    replaceAll("\\.", "");
-            if(! text.isEmpty()) {
-                Integer count = map.get( text );
-                if(count == null) {
+                    replaceAll ( " " , "" ).
+                    replaceAll ( "\\!" , "" ).
+                    replaceAll ( "\\?" , "" ).
+                    replaceAll ( "\n" , "" ).
+                    replaceAll ( "\\-" , "" ).
+                    replaceAll ( "\\." , "" );
+            if ( ! text.isEmpty ( ) ) {
+                Integer count = map.get ( text );
+                if ( count == null ) {
                     count = 0;
                 }
-                map.put( text , ++count);
+                map.put ( text , ++ count );
             }
         }
-        for(String word : map.keySet()) {
-            System.out.println(word + ": " + map.get(word));
+        for ( String word : map.keySet ( ) ) {
+            System.out.println ( word + ": " + map.get ( word ) );
 
         }
-        Integer max = map.entrySet()
-                .stream()
-                .max((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1)
-                .get()
-                .getValue();
+        Integer max = map.entrySet ( )
+                .stream ( )
+                .max ( ( entry1 , entry2 ) -> entry1.getValue ( ) > entry2.getValue ( ) ? 1 : - 1 )
+                .get ( )
+                .getValue ( );
 
-        List listOfMax = map.entrySet()
-                .stream()
-                .filter(entry -> entry.getValue() == max)
-                .map(Map.Entry::getKey)
-                .collect( Collectors.toList());
+        List listOfMax = map.entrySet ( )
+                .stream ( )
+                .filter ( entry -> entry.getValue ( ) == max )
+                .map ( Map.Entry :: getKey )
+                .collect ( Collectors.toList ( ) );
 
-        System.out.println ("------------------------------------------------------------------------------" );
+        System.out.println ( "------------------------------------------------------------------------------" );
 
-        System.out.println("Слово, которое встречается чаще чем остальные - " + listOfMax +
-                ", " + "повторяется " + max + " раз(а)");
+        System.out.println ( "Слово, которое встречается чаще, чем остальные - " + listOfMax +
+                ", " + "повторяется " + max + " раз(а)" );
 
 
     }
